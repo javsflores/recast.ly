22e9ba7fdcf267479123cb341b319fcb08f4cf35
Javier Flores <javier.floresapodaca@gmail.com>
2020-09-02 01:34:41 -0700

    Made Progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -7,10 +10,10 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <VideoPlayer video={window.exampleVideoData[0]}/>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos={window.exampleVideoData}/>
       </div>
     </div>
   </div>
@@ -19,3 +22,6 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
+
+// <div><h5><em>videoList</em> view goes here</h5></div>
